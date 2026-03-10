d5e67199f55c2df3d029bc28b638befeea886be2
yugendra achary <yugendrachari09@gmailcom>
2026-03-10 14:16:31 +0530

    updated server configuration for Railway deployment

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-}); 
+    console.log(`Server is running on port ${PORT}`);
+});
